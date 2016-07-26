972193444c1f6a92ba7c35932603262f4daef01b
wayoflife <dbetsche@gmail.com>
2016-07-26 22:02:03 +0200

    classifier ids auslesbar über install-classifier.js

--- dialogs/install-classifier.js
+++ dialogs/install-classifier.js
@@ -16,166 +16,38 @@ var async = require('async'),
     bluemix = require('../config/bluemix'),
     fs = require('fs'),
     path = require('path'),
-    request = require('request'),
+    request = require('request'),  // https://github.com/request/request
     extend     = require('util')._extend;
 
-var dialogService = {
-    getDialogs: function(credentials, cb) {
+var nlcService = {
+    getClassifiers: function(credentials) {
+
         request({
-            url: credentials.url + '/v1/dialogs',
+            url: credentials.url + '/v1/classifiers',
             auth: {
                 user: credentials.username,
                 pass: credentials.password
             }
         },
+
         function(error, response, body) {
             if (error) {
-                cb(error);
+                console.log('error in retrieving classifiers: ', error);
             } else {
-                var dialogs = {};
+                var classifiers = {};
                 var jsonBody = JSON.parse(body);
-                if(jsonBody.dialogs){
-                    jsonBody.dialogs.forEach(function (el) {
-                        dialogs[el.name] = el.dialog_id;
+                if(jsonBody.classifiers){
+                    jsonBody.classifiers.forEach(function (element) {
+                        classifiers[element.created] = element.classifier_id;
                     });
                 }
-                cb(null, dialogs);
+                console.log('all received classifiers: ', JSON.stringify(classifiers));
+                console.log(body);
+                // TODO write classifiers to json file
             }
         });
-    },
-    setDialog: function(credentials, name, id, fullPath, cb) {
-        var url,
-            method,
-            fileName = name + '.xml',
-            formData = {
-                file: {
-                    value: fs.createReadStream(fullPath),
-                    options: {
-                        filename: fileName
-                    }
-                }
-            };
-        if (id === 'pending') {
-            url = credentials.url + '/v1/dialogs';
-            method = 'POST';
-            formData.name = name;
-        } else {
-            url = credentials.url + '/v1/dialogs/' + id;
-            method = 'PUT';
-        }
-        request({
-                method: method,
-                url: url,
-                auth: {
-                    user: credentials.username,
-                    pass:credentials.password
-                },
-                formData: formData
-            },
-            function(error, response, body) {
-
-                if (error) {
-                    cb(error);
-                } else {
-                    if (response.statusCode < 300) {
-                        if (response.statusCode === 201) {
-                            var info = JSON.parse(body);
-                            id = info.dialog_id;
-                        }
-                        var pair = {};
-                        pair[name] = id;
-                        cb(null, pair);
-                    } else {
-                        cb(new Error(':-( HTTP status: ' + response.statusCode));
-                    }
-                }
-            });
     }
-};
-
-var dialogFiles = (function() {
-    var fileInfo = {};
-    var thisDir = path.dirname(__filename);
-
-    function getXmlFilesInThisDir() {
-        var xmlFiles = fs.readdirSync(thisDir).filter(function(aFile) {
-            var fp = path.join(thisDir, aFile);
-            return !fs.statSync(fp).isDirectory() &&
-                   path.extname(fp).match(/^\.xml$/i);
-        });
-        return xmlFiles.map(function(aFile) {return aFile.replace(/\.xml$/i, '');});
-    }
-
-    return {
-        loadFromDisk: function(userName) {
-            var suffix = '_' + userName.substring(0, 8);
-            fileInfo = {};
-            getXmlFilesInThisDir().forEach(function(name) {
-                var key = name + suffix;
-                var fp = path.join(thisDir, name + '.xml');
-                fileInfo[key] = {id: 'pending', fullPath: fp};
-            });
-        },
-        updateWithServiceInfo: function(serviceInfo) {
-            for (var key in fileInfo) {
-                if (fileInfo.hasOwnProperty(key)) {
-                    if (serviceInfo[key]) {
-                        fileInfo[key].id = serviceInfo[key];
-                    }
-                }
-            }
-        },
-        get: function() {return fileInfo;}
-    };
-})();
-
-
-function updateDialogFiles(credentials) {
-    function allDialogsProcessedCb(error) {
-        if (error) {
-            console.error('done :-(');
-        } else {
-            var dialogsFile = path.join(path.dirname(__filename), 'dialog-id.json');
-            fs.writeFileSync(dialogsFile,
-                             JSON.stringify(dialogFiles.get(), null, 4));
-            console.log('dialog files processed');
-        }
-    }
-
-    function getDialogsCb(error, installed) {
-        if (error) {
-            console.error('something went wrong fetching dialogs: ' + error);
-        } else {
-            dialogFiles.loadFromDisk(credentials.username);
-            dialogFiles.updateWithServiceInfo(installed);
-
-            var processDialog = function(value, name, cb) {
-                var dialogProcessedCb = function(error, pair) {
-                    if (error) {
-                        console.error('something went wrong with dialog "' + name + '": ' + error);
-                        cb(error);
-                    } else {
-                        dialogFiles.get()[name].id = pair[name];
-                        cb();
-                    }
-                };
-                dialogService.setDialog(credentials, name, value.id, value.fullPath,
-                                        dialogProcessedCb);
-            };
-            async.forEachOf(dialogFiles.get(),
-                            processDialog,
-                            allDialogsProcessedCb);
-        }
-    }
-
-    if (credentials && credentials.username) {
-        dialogService.getDialogs(credentials, getDialogsCb);
-    } else {
-        console.warn('WARNING: Missing credentials or VCAP_SERVICES. Not installing dialog files.');
-    }
-}
-
-
+  }
 
 var nlccredentials =  extend({
   url : 'https://gateway.watsonplatform.net/natural-language-classifier/api',
@@ -192,5 +64,4 @@ try {
     console.log('credentials json not existent')
 }
 
-//TODO Methode ändern um nlc-id auszulesen
-updateDialogFiles(nlccredentials);
+nlcService.getClassifiers(nlccredentials);
